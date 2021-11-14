6a3c364777f24ee7552f2954fe87072d388b9677
Nick Janes <njanes@ucsc.edu>
2021-11-14 15:37:36 -0800

    Added caching to reduce api calls

--- nfts.py
+++ nfts.py
@@ -3,15 +3,26 @@ import requests, json, random
 #rather than get a random list of all nfts we just pull from a few popular colections for simplicity
 #this feature would be accounted for in a final version
 addresses = [
-    '0xe4605d46fd0b3f8329d936a8b258d69276cba264',
-    '0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb',]
+    '0x60e4d786628fea6478f785a6d7e704777c86a7c6',
+    '0xe4605d46fd0b3f8329d936a8b258d69276cba264',]
 
 #Input
 #   -address - a crypto collection address with nfts on opensea
 #Output
 #   returns a json object containing a list of nft token ids
-def get_nft_token_ids(address):
-    return requests.get('https://api.covalenthq.com/v1/1/tokens/' + address + '/nft_token_ids/?key=ckey_644cc507cd5946cb82e4c1e96e5').json()
+def update_nft_token_ids():
+    for address in addresses:
+        body = requests.get('https://api.covalenthq.com/v1/1/tokens/' + address + '/nft_token_ids/?key=ckey_644cc507cd5946cb82e4c1e96e5').json()
+        f = open(address + '.json', 'w', encoding='utf-8')
+        json.dump(body, f, ensure_ascii=False, indent=4)
+
+#Input
+#   -address - a crypto collection address with nfts on opensea
+#Output
+#   returns a json object containing a list of nft token ids
+def load_nft_token_ids(address):
+    f = open(address + '.json', 'r', encoding='utf-8')
+    return json.load(f)
 
 #Input
 #   -address - a crypto collection address with nfts on opensea
@@ -34,15 +45,23 @@ def get_nft_metadata(address, token_id):
 #Output
 #   returns an object for an nft with its name, image link, and original minting hash
 def get_random_nft():
-    collection = get_nft_token_ids(addresses[random.randrange(len(addresses))])         # returns a list of all tokens for a given randomized collection from our list
-    r = random.randrange(int(collection['data']['pagination']['total_count']))          # random token_id out of the collection
-    
-    nft = get_nft_metadata(addresses[0], r)                                             #gets nft data for token id
-    name = nft['data']['items'][0]['nft_data'][0]['external_data']['name']
-    image = nft['data']['items'][0]['nft_data'][0]['external_data']['image']
+    address = addresses[random.randrange(len(addresses))]
+    collection = load_nft_token_ids(address)                                                # returns a list of all tokens for a given randomized collection from our list
+    token = random.randrange(int(collection['data']['pagination']['total_count']))          # random token_id out of the collection
 
-    transactions = get_nft_transactions(addresses[0], r)        
-    hash = transactions['data']['items'][0]['nft_transactions'][0]['tx_hash']           # gets hash for original minting
+    #print("Grabbing nft " + str(token) + "/" + str(collection['data']['pagination']['total_count']) + " from collection of address " + str(address))
     
-    result = {'name': name, 'image':image, 'hash': hash}
+    nft = get_nft_metadata(address, token)                                                  #gets nft data for token id
+    
+    #print(json.dumps(nft['data']['items'][0]['nft_data'][0]['external_data'], indent=4))
+    
+    name = nft['data']['items'][0]['nft_data'][0]['external_data']['name']
+    image = nft['data']['items'][0]['nft_data'][0]['external_data']['image_512']
+
+    transactions = get_nft_transactions(address, token)        
+    hash = transactions['data']['items'][0]['nft_transactions'][0]['tx_hash']               # gets hash for original minting
+    
+    result = {'name': name, 'token': token, 'image':image, 'hash': hash}
     return result
+
+print(get_random_nft())
