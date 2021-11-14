5501ca05c41ead0a2c70e24e5ecc04ac45f1a2f0
Nick Janes <njanes@ucsc.edu>
2021-11-14 15:54:07 -0800

    updated base urls/views for pokemon nft app

--- website/nftpokemon/views.py
+++ website/nftpokemon/views.py
@@ -2,18 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    directory = 'summoner_info/ranked_info/'
-    summoners = []
-    summoner_names = []
 
-    context = {'summoner_names': summoner_names, 'summoner_stats': summoners}
+    context = {}
 
     return render(request, 'index.html', context)
 
-def display_summoner(request, summoner_name):
-    directory = 'summoner_info/ranked_info/'
-    summoner_stats = json.load(open(directory + summoner_name + ".json"))
-
-    context = {'name': summoner_name, 'summoner_stats': summoner_stats}
-
+def battle(request, summoner_name):
+    
+    context = {}
     return render(request, 'summoner_detail.html', context)
