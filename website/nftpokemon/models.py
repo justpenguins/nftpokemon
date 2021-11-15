4623d91648393d63d9b2102e32403dd72666c729
Nick Janes <njanes@ucsc.edu>
2021-11-14 17:09:12 -0800

    set up basic model to save nfts

--- website/nftpokemon/models.py
+++ website/nftpokemon/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class NFT(models.Model):
+    name = models.CharField(max_length=200)
+    token = models.IntegerField()
+    img = models.CharField(max_length=200)
+    hash = models.CharField(max_length=200)
