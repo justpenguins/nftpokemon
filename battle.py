82020acc5bb45f35557997b96dec2989bae3ac65
Cam Lee <cam.lee@mail.ubc.ca>
2021-11-14 17:09:35 -0800

    updated battle backend

--- battle.py
+++ battle.py
@@ -1,22 +1,37 @@
-from algorithm import *
+from algorithm import get_stats, get_stat, get_type
 
 
 class Battle:
     playerHP = 0 
     enemyHP = 0
+    playerAtk = 0
+    enemyAtk = 0
     animations = []
+
+    def __init__(self, php, ehp, patk, eatk):
+        self.playerHP = php
+        self.enemyHP = ehp
+        self.playerAtk = patk
+        self.enemyAtk = eatk
 
     # player hash1 attacks hash2
     def attack(hash1, hash2):
-        hp1 = hash1.get_hp()
-        hp2 = hash2.get_hp()
+        atk = hash1.get_atk()
+        hp = hash2.get_hp()
 
-    def round():
+        hp = hp - atk
+            
+
+    def round(enemyHP, playerHP):
+        turn = 0
         while (enemyHP > 0 and playerHP > 0):
             if turn == 1: # player attacks
                 attack(player, enemy)
+                turn = 0
             else:         # enemy attacks
-                attack(enemy, Player)
+                attack(enemy, player)
+                turn = 1
+
 
 
 class Player:
@@ -24,9 +39,9 @@ class Player:
     player_hp, player_atk, player_def = 0
     player_type = ""
 
-    def __init__(self, hash):
-        stats = get_stats(hash)
-        self.player_id = hash
+    def __init__(self, hashid):
+        stats = get_stats(hashid)
+        self.player_id = hashid
         self.player_hp = stats[0]
         self.player_atk = stats[1]
         self.player_def = stats[2]
